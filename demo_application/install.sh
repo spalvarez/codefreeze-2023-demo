0e1d87a5871728418f5e4ee8738eb35675f6116b
Sean Alvarez <seanalvarez@ritchiebros.com>
2023-01-10 14:42:31 -0500

    Add OPA Gatekeeper Demo
    Signed-off-by: Sean Alvarez <seanalvarez@ritchiebros.com>

--- demo_application/install.sh
+++ demo_application/install.sh
@@ -3,4 +3,3 @@
 kubectl create -f https://raw.githubusercontent.com/cilium/cilium/1.12.5/examples/minikube/http-sw-app.yaml
 sleep 3
 kubectl get pods,svc
-
---
+++ opa/install gatekeeper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
+helm install gatekeeper/gatekeeper --name-template=gatekeeper --namespace gatekeeper-system --create-namespace
